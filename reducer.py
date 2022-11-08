3d5b907283445764d306b855f513ccdb8f2216c8
michalasia10 <misiek1027@o2.pl>
2022-11-08 19:18:44 +0100

    hadoop

---
+++ reducer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+
+current_year_month_location = None
+current_count = 0
+year_month_location = None
+
+for line in sys.stdin:
+
+    line = line.strip()
+
+    year_month_location, count = line.split('\t', 1)
+
+    try:
+        count = int(count)
+    except ValueError:
+
+        continue
+
+    if current_year_month_location == year_month_location:
+        current_count += count
+    else:
+        if current_year_month_location:
+            # write result to STDOUT
+            try:
+                st = current_year_month_location.split(':')
+                print('%s\t%s\t%s\t%s' % (st[0], st[1], st[2], current_count))
+            except AttributeError:
+                pass
+        current_count = count
+        current_year_month_location = year_month_location
+
+# do not forget to output the last year_month_location if needed!
+if current_year_month_location == year_month_location:
+    try:
+        st = current_year_month_location.split(':')
+        print('%s\t%s\t%s\t%s' % (st[0], st[1], st[2], current_count))
+    except AttributeError:
+        pass
